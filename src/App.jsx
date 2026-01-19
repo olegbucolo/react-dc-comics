523482b4e3d1351d0c5b22df4d0b7d7e0f2e4ac7
olegbucolo <olegbucolo97@gmail.com>
2026-01-19 16:13:24 +0100

    Feature[BannerMain]: Add BannerMain and style

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HeaderMain } from './components/HeaderMain';
 import { MainMain } from './components/MainMain';
 import { FooterMain } from './components/FooterMain';
 import { JumboMain } from './components/JumboMain';
+import { BannerMain} from './components/BannerMain'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
       <HeaderMain />
       <JumboMain />
       <MainMain />
+      <BannerMain />
       <FooterMain />
     </>
   )
---
+++ src/components/BannerMain.jsx
@@ -0,0 +1,31 @@
+import './BannerMain.css'
+
+export function BannerMain() {
+    return (
+        <section className="section-banner">
+            <ul>
+                <li>
+                    <img src="./img/buy-comics-digital-comics.png" alt="" />
+                    <h3>Digital Comics</h3>
+                </li>
+                <li>
+                    <img src="./img/buy-comics-merchandise.png" alt="" />
+                    <h3>DC merchandise</h3>
+                </li>
+                <li>
+                    <img src="./img/buy-comics-subscriptions.png" alt="" />
+                    <h3>subscriptioin</h3>
+                </li>
+                <li>
+                    <img src="./img/buy-comics-shop-locator.png" alt="" />
+                    <h3>comic shop locator</h3>
+                </li>
+                <li>
+                    <img src="./img/buy-dc-power-visa.svg" alt="" />
+                    <h3>dc power visa</h3>
+                </li>
+            </ul>
+
+        </section>
+    )
+}
