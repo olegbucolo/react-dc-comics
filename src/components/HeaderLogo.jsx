6a14388cd466e65a699c86eec8cad5a08b5c6bcf
olegbucolo <olegbucolo97@gmail.com>
2026-01-19 17:24:01 +0100

    refactor[Header, footer]: Divide Header navbar in components, add cursor: pointer to the button in the footer

---
+++ src/components/HeaderLogo.jsx
@@ -0,0 +1,9 @@
+import dcLogo from '../../public/img/dc-logo.png';
+
+export function HeaderLogo() {
+    return (
+        <div className="div-logo-header-main">
+            <a href=""><img src={dcLogo} alt="" /></a>
+        </div>
+    )
+}
--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,25 +1,13 @@
 import './HeaderMain.css';
-import dcLogo from '../../public/img/dc-logo.png';
+import { HeaderLogo } from './HeaderLogo';
+import { HeaderList } from './HeaderList';
 
 export function HeaderMain() {
     return (
         <header className="header-main">
             <nav className="nav-header-main">
-                <div className="div-logo-header-main">
-                    <a href=""><img src={dcLogo} alt="" /></a>
-                </div>
-                <ul className="ul-header-main">
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">Tv</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Collectibles</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Shop</a></li>
-                </ul>
+                <HeaderLogo />
+                <HeaderList />
             </nav>
         </header>
     )
