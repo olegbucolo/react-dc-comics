669bf572ee6a5225e76bc33aa8c74e0bd1a135f7
olegbucolo <olegbucolo97@gmail.com>
2026-01-20 16:16:46 +0100

    Feat: Create data in src/data for the header component

---
+++ src/data/headerList.js
@@ -0,0 +1,12 @@
+[
+    {id: 1, title: "characters", anchor: "#"},
+    {id: 2, title: "comics", anchor: "#"},
+    {id: 3, title: "movies", anchor: "#"},
+    {id: 4, title: "tv", anchor: "#"},
+    {id: 5, title: "games", anchor: "#"},
+    {id: 6, title: "collectibles", anchor: "#"},
+    {id: 7, title: "videos", anchor: "#"},
+    {id: 8, title: "fans", anchor: "#"},
+    {id: 9, title: "news", anchor: "#"},
+    {id: 10, title: "shop", anchor: "#"},
+]
